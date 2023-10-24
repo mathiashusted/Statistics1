1edc5453b239fdd753891165abe193f7d4e17fea
Mathias Marryat Husted <mathias@87-77-6-124.mna.fu-berlin.de>
2023-10-24 17:45:26 +0200

    Tut 1

---
+++ src/tut/ue1.R
@@ -0,0 +1,27 @@
+40*5
+4*(3.76 + 4.78)
+pi*sin(2*pi/3)
+sqrt(2+log(3.45,10))
+2^(2.3451)*exp((6.8)*sin(pi/32)+67)
+
+v0 = c(2,3,4,5)
+v1 = c(1,2,5,11,-10.5)
+v2 = c('B','i','o','I','n','f','o','r','m','a','t','i','k')
+v3 = c("Bio","Informatik")
+v4 = c(TRUE,FALSE,FALSE,FALSE,FALSE,TRUE)
+
+v5 = rep(1,100)
+
+v7 <- seq(0, -333, by=-3)
+
+v8 <- seq(from=-15, to=15, length.out=43)
+
+v9 <- v8[c(1,39)]
+
+v10 <- v7[v7<41]
+
+v11 <- v7[c(TRUE,FALSE)]
+
+v12 = v7[1:2]
+
+print(v12)
