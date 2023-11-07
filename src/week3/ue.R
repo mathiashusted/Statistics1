5f0ffd4e67069e20713fce01581ae53232496ab4
Mathias Marryat Husted <mathias@87-77-6-124.mna.fu-berlin.de>
2023-11-07 15:01:10 +0100

    Week 3 done

--- src/week3/ue.R
+++ src/week3/ue.R
@@ -12,3 +12,19 @@ prevalence <- c(0.1, 0.5, 0.01, 0.001) # a, b, c, d
 posPred <- (sensitivity * prevalence)/((sensitivity*prevalence) + (1-specifity)*(1-prevalence))
 negPred <- (specifity*(1-prevalence))/((specifity * (1-prevalence)) + (1-sensitivity) * prevalence)
 
+
+
+# Aufgabe 12: Plotten Sie mit R in einem Liniendiagramm die Abhängigkeit des
+# positiven prädiktiven Werts von der Prävalenz im Bereich von 0% bis 100% für
+
+# A: Sens = 0.8, Spez = 0.9
+# B: Sens = 0.8, Spez = 0.99
+# C: Sens = 0.99, Spez = 0.8
+
+diaSensitivity <- c(0.8, 0.8, 0.99)
+diaSpecifity <- c(0.9, 0.99, 0.8)
+
+
+for (x in 1:3) {
+  plot((diaSensitivity[x] * 1:100)/((diaSensitivity[x]*1:100) + (1-diaSpecifity[x])*(1-1:100)), type="l", col="red", ylab="pos. prädiktiver Wert")
+}
